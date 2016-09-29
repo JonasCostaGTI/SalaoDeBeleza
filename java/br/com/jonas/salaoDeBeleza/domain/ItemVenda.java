26763f5333df3311f067744f3ee9306c580c6102
Jonas <jonas.costa@icloud.com>
2016-09-29 19:52:46 -0300

    item venda

---
+++ java/br/com/jonas/salaoDeBeleza/domain/ItemVenda.java
@@ -0,0 +1,90 @@
+package br.com.jonas.salaoDeBeleza.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+
+@Entity
+public class ItemVenda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	@Column(nullable = false)
+	private Short quantidade;
+	
+	@Column(nullable = false, precision = 7, scale = 2)
+	private BigDecimal valorParcial;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Produto produto;
+	
+	
+	// e agora? Venda ou Funcionario?
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Venda venda;
+
+
+	public Short getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(Short quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	public BigDecimal getValorParcial() {
+		return valorParcial;
+	}
+
+
+	public void setValorParcial(BigDecimal valorParcial) {
+		this.valorParcial = valorParcial;
+	}
+
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+
+	public Venda getVenda() {
+		return venda;
+	}
+
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+
+}
